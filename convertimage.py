784539ae09ccefb2570d2f568f51e08288e2e64b
tutancamom <jonnys.gomes@outlook.com>
2024-05-05 13:17:10 -0700

    em desenvolvimento

--- convertimage.py
+++ convertimage.py
@@ -16,21 +16,22 @@ def chash():
     img = input('Selecione a imagem: \n')
     
     print(f'Imagem selecionada: {list_file[i].name}\n')
-    op = input('Deseja continuar: [Y] ou [N] ?')
+    op = int(input('Deseja continuar:\n[1] SIM\n[2] NAO\n'))
 
-    str_image = base64.b64encode(img) if op == 'Y' else print('Programa Finalizado...')
+    str_image = base64.b64encode(img) if op == "Y" else print('Programa Finalizado...')
 
-    opt = input('Imagem convertida, deseja salvar Hash em arquivo: [Y] ou [N] ?')
+    opt = int(input('Imagem convertida, deseja salvar Hash em arquivo:\n[1] SIM\n[2] NAO\n'))
+    img_name = str(list_file[i].name)
 
+    saveFile(str_image, img_name) if opt == 1 else print('Programa Finalizado...')
 
-    saveFile(str_image, img) if opt == 'Y' else print('Programa Finalizado...')
+def saveFile(hash, img_name):
+    file_name= (f'hash_from_{img_name}.txt')
+    print(file_name)
+    file_save =  open(file_name,'a')
+    file_save.write(str(hash))
 
-def saveFile(hash, img):
-    file_name= (f'hash_from_{img}.txt')
-    with open(file_name,'a') as file_save:
-        file_save.write(hash)
-
-    print(f'Hash da Imagem {img}, salvo no arquivo: {file_save}')
+    print(f'Hash da Imagem {img_name}, salvo no arquivo: {file_name}')
 
 def main():
     option = int(input('[1] Converter Imagem emm Hash \n[2] Converter Hash emm Imagem'))
